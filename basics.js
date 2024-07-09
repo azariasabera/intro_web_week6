6f5aa18b70971cfe21370858b259b59b77116484
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-09 11:47:54 +0300

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,101 +1,82 @@
-const jsonQuery = { 
-    "query": [{   
-        "code": "Vuosi",
-        "selection": {
-            "filter": "item",
-            "values": [ "2000",
-                        "2001",
-                        "2002",
-                        "2003",
-                        "2004",
-                        "2005",
-                        "2006",
-                        "2007",
-                        "2008",
-                        "2009",
-                        "2010",
-                        "2011",
-                        "2012", 
-                        "2013",
-                        "2014",
-                        "2015",
-                        "2016",
-                        "2017",
-                        "2018",
-                        "2019",
-                        "2020",
-                        "2021" ]}
-            },
-            {
-                "code": "Alue", 
-                "selection": {
-                    "filter": "item",
-                    "values": ["SSS"]
-                }
-            },
-            {
-                "code": "Tiedot",
-                "selection": {
-                    "filter": "item",
-                    "values": ["vaesto"]
-                }
-            }],
-    "response": { "format": "json-stat2"}
-}
+const jsonQuery = {
+    "query": [
+        {
+            "code": "Vuosi",
+            "selection": {
+                "filter": "item",
+                "values": [
+                    "2000", "2001", "2002", "2003", "2004", "2005", "2006", 
+                    "2007", "2008", "2009", "2010", "2011", "2012", "2013", 
+                    "2014", "2015", "2016", "2017", "2018", "2019", "2020", 
+                    "2021"
+                ]
+            }
+        },
+        {
+            "code": "Alue",
+            "selection": {
+                "filter": "item",
+                "values": ["SSS"]
+            }
+        },
+        {
+            "code": "Tiedot",
+            "selection": {
+                "filter": "item",
+                "values": ["vaesto"]
+            }
+        }
+    ],
+    "response": {
+        "format": "json-stat2"
+    }
+};
 
 const getData = async () => {
-    const url = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"
-
+    const url = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px";
+    
     const res = await fetch(url, {
         method: "POST",
-        headers: {"content-type": "application/json"},
+        headers: {
+            "content-type": "application/json"
+        },
         body: JSON.stringify(jsonQuery)
-    })
-    if(!res.ok) {
+    });
+    
+    if (!res.ok) {
         return;
     }
-    const data = await res.json()
-
-    return data
-}
+    
+    const data = await res.json();
+    return data;
+};
 
 const buildChart = async () => {
-    const data = await getData()
+    const data = await getData();
     
-    const area = Object.values(data.dimension.Alue.category.label);
     const years = Object.values(data.dimension.Vuosi.category.label);
     const values = data.value;
 
-    // make chart data
+    console.log(years, values);
+
+    // Construct chart data
     const chartData = {
         labels: years,
-        datasets: [] // accepts an array of objects with name and values
-    }
-    for (let i = 0; i < area.length; i++) {
-        const dataset = {
-            name: area[i],
-            values: []
-        }
-        for (let j = 0; j < years.length; j++) {
-            dataset.values.push(values[i + j])
-        }
-        chartData.datasets.push(dataset)
-    }
+        datasets: [
+            {
+                name: "Population",
+                values: values
+            }
+        ]
+    };
 
     const chart = new frappe.Chart("#chart", {
-        title: "Population growth of municipalities in Finland",
+        title: "Population growth of Finland",
         data: chartData,
         type: "line",
         height: 450,
-        colors: ['#eb5146'],
-        /*barOptions: {
-           stacked: 1
-        }
-        lineOptions: {
-            hideDots: 1,
-            regionFill: 0
-        }*/
-
+        colors: ['#eb5146']
     });
-}
-buildChart()
+};
+
+buildChart();
